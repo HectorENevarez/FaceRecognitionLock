4303ad7aa8b2933f8014864a46314bef691fcd62
mjturi <56698233+mjturi@users.noreply.github.com>
2020-12-06 16:52:53 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import socket
 from PIL import Image
+import face_detection_small
 
-#192.168.0.103
+
+# 192.168.0.103
 def client_program():
     host = '192.168.0.75'  # as both code is running on same pc
     port = 2345  # socket server port number
@@ -21,6 +23,7 @@ def client_program():
 
     client_socket.close()  # close the connection
 
+
 def server_program():
     host = '0.0.0.0'
     port = 2345
@@ -31,31 +34,31 @@ def server_program():
 
     c, addr = s.accept()
     print("Got connection from: ", addr)
-    l = c.recv(1024)
-    while(l):
-        print("Recieving...")
-        f.write(l)
-        l = c.recv(1024)
-    #print(str(f))
+    l = c.recv(40960000)
+    f.write(l)
+    print("test")
     f.close()
     print("Done Recieving")
-    #c.send(b"Thank you for connecting")
-    c.close()
+    # c.send(b"Thank you for connecting")
+
 
     print("Converting raw Image...")
     im = Image.open("torecv.raw")
     rgb_im = im.convert('RGB')
     rgb_im.save("converted.jpg")
     print("Image has been converted to jpg")
-    #rawData = open("torecv.txt", 'rb').read()
-    #imgSize = (854, 642)
-    #img = Image.frombytes('L', imgSize, rawData)
-    #img.save("torecv.png")
+    # rawData = open("torecv.txt", 'rb').read()
+    # imgSize = (854, 642)
+    # img = Image.frombytes('L', imgSize, rawData)
+    # img.save("torecv.png")
 
-            
+    result = face_detection_small.classify_face("converted.jpg")
+    print("sending result")
+    c.send(str(result).encode())
 
+    c.close()
 
 if __name__ == '__main__':
-    #client_program()
+    # client_program()
     server_program()
 
